08d8583766ad19abb68d27f2721b4c8a8820c04c
unknown <pedrorembold@gmail.com>
2022-10-07 12:03:02 -0300

    filro adicionado

--- src/componentes/formulario/index.js
+++ src/componentes/formulario/index.js
@@ -4,7 +4,7 @@ import CampoTexto from '../CampoTexto'
 import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
-const Formulario = () => {
+const Formulario = (props) => {
     const times = [
         'Programação',
         'Front-End',
@@ -21,7 +21,12 @@ const [time, setTime] = useState('');
     
     const aoSalvar = (evento) => {
         evento.preventDefault()
-        console.log('Form foi submetido', nome, cargo, imagem, time);
+        props.aoColaboradorCadastrado({
+            nome,
+            cargo,
+            imagem,
+            time
+        });
     }
 
     return(
